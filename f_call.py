062419adbcedeb16d535f94eabc50424e0a38785
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-07 15:34:24 +0530

    Create f_call.py

---
+++ f_call.py
@@ -0,0 +1,16 @@
+#FUNCTION ARE TWO PARS 
+#example of function
+#def is key worgd
+def avg(n1,n2,n3):   #parameters
+    return (n1+n2+n3)/3.0
+print("welcome")
+result1=avg(10,20,30)    # function call
+result2=avg(1,2,3)
+result3=avg(3.1,2.4,4.5)
+
+print(result1)
+print(result2)
+print(result3)
+
+
+
